98cb44b0599058c68397738111a5b79cd57457a6
Algo <arash.torabi_goodarzi@stud-mail.uni-wuerzburg.de>
2025-06-18 11:13:32 +0200

    hypernet init

--- hypercez/hypernet/__init__.py
+++ hypercez/hypernet/__init__.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 from torch import nn
 
 from hypercez import Hparams
-from hypercez.hypernet.hypercl import ChunkedHyperNetworkHandler, HyperNetwork
+from hypercez.hypernet.hypercl import ChunkedHyperNetworkHandler, HyperNetwork, MainNetInterface
 from hypercez.util.format import str_to_act
 
 
@@ -37,3 +37,36 @@ def build_hnet(hparams: Hparams, model: nn.Module, hnet_type: HNet_Type = HNet_T
             activation_fn=str_to_act(hparams.hnet_act)
         )
 
+    setattr(
+        hparams,
+        "num_weights_class_hyper_net",
+        sum(p.numel() for p in hnet.parameters() if p.requires_grad)
+    )
+    setattr(
+        hparams,
+        "num_weights_class_net",
+        MainNetInterface.shapes_to_num_weights(param_shape_lst)
+    )
+    setattr(
+        hparams,
+        "compression_ratio_class",
+        hparams.num_weights_class_hyper_net / hparams.num_weights_class_net
+    )
+
+    for W in list(hnet.parameters()):
+        if W.ndimension() == 1: # Bias vector.
+            nn.init.constant_(W, 0)
+        elif hparams.hnet_init == "normal":
+            nn.init.normal_(W, mean=0, std=hparams.std_normal_init)
+        elif hparams.hnet_init == "xavier":
+            nn.init.xavier_uniform_(W)
+
+    if hasattr(hnet, 'chunk_embeddings'):
+        for emb in hnet.chunk_embeddings:
+            nn.init.normal_(emb, mean=0, std=hparams.std_normal_cemb)
+
+    if hparams.use_hyperfan_init:
+        if hparams.model.startswith("hnet"):
+            hnet.apply_hyperfan_init(temb_var=hparams.std_normal_temb**2)
+
+    return hnet
