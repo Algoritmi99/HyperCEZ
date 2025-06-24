5be98c0aadc504681087f75f8e6187717adc361e
Algo <arash.torabi_goodarzi@stud-mail.uni-wuerzburg.de>
2025-06-24 16:20:43 +0200

    planning next steps

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ def main():
 if __name__ == "__main__":
     main()
 
-# TODO:
-#   1. train file that trains an arbitrary agent and resembles the HyperCRL trainer from outside
-#   and EfficientZeroV2 trainer from inside for the agent.
-#       --> An idea is object oriented training subroutines within the agent class of each agent.
-#   2. a run script that starts the training routine for any given parameters (Agent, Env, etc.)
-#   3. The main file must take parameters and serve as the API for the entire program
+    # TODO:
+    #   1. train file that trains an arbitrary agent and resembles the HyperCRL trainer from outside
+    #   and EfficientZeroV2 trainer from inside for the agent.
+    #       --> An idea is object oriented training subroutines within the agent class of each agent.
+    #   2. a run script that starts the training routine for any given parameters (Agent, Env, etc.)
+    #   3. The main file must take parameters and serve as the API for the entire program
