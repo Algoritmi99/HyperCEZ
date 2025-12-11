fabdfc35fa1e530c3e82cd082c596dd084b7cdb5
algorithmi <arash.goodarzi@uni-wuerzburg.de>
2025-12-11 09:00:58 +0100

    debugging training

--- hypercez/trainer.py
+++ hypercez/trainer.py
@@ -76,14 +76,11 @@ class Trainer:
                     x_t, _ = env.reset()
                     self.agent.reset(x_t)
 
-                if it % 50 == 0:
-                    self.agent.save("./agents/" + self.hparams.env + "/agent_" + str(it) + agent_name + ".pth")
-                    self.plotter.save_raw_data()
-
                 if train_cnt % self.hparams.train["eval_interval"] == 0 and evaluate and not evaluated:
                     evaluator = Evaluator(self.agent, self.hparams, plotter=self.plotter)
                     evaluator.evaluate(self.hparams.train["eval_n_episode"], pbar=pbar)
                     self.agent.save("./agents/" + self.hparams.env + "/agent_" + str(it) + agent_name + ".pth")
+                    self.plotter.save_raw_data()
                     evaluated = True
 
                 it += 1
