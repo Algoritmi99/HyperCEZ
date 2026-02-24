1bb7decfb78e70e3730a3e6f1382e1cf598dfe0c
algoritmi <arash.goodarzi@uni-wuerzburg.de>
2026-02-24 10:45:33 +0100

    Revised HyperCEZAgent

--- hypercez/agents/hypercez_agent.py
+++ hypercez/agents/hypercez_agent.py
@@ -11,6 +11,7 @@ from hypercez.hypernet import build_hnet
 from hypercez.hypernet.hypercl.utils import hnet_regularizer
 from hypercez.hypernet.hypercl.utils import ewc_regularizer as ewc
 from hypercez.util.hypercez_data_mng import HyperCEZDataManager
+from hypercez.hypernet.hypercl.utils import optim_step as opstep
 
 
 class HyperCEZAgent(Agent):
@@ -194,6 +195,7 @@ class HyperCEZAgent(Agent):
         emb_optimizers = {}
         reg_param_map = {}
         schedulers = {}
+        scaler = torch.amp.GradScaler()
         for component_name in self.hnet_component_names:
             # Regularizer Targets
             targets[component_name] = hnet_regularizer.get_current_targets(task_id, self.hnet_map[component_name])
@@ -229,8 +231,10 @@ class HyperCEZAgent(Agent):
             schedulers[theta_optimizers[component_name]] = make_scheduler(theta_optimizers[component_name], self.hparams)
             schedulers[emb_optimizers[component_name]] = make_scheduler(emb_optimizers[component_name], self.hparams)
 
-        self.cl_training_misc[task_id] = (targets, theta_optimizers, emb_optimizers, reg_param_map, fisher_est_map, schedulers)
-        return targets, theta_optimizers, emb_optimizers, reg_param_map, fisher_est_map, schedulers
+        self.cl_training_misc[task_id] = (
+            targets, theta_optimizers, emb_optimizers, reg_param_map, fisher_est_map, schedulers, scaler
+        )
+        return targets, theta_optimizers, emb_optimizers, reg_param_map, fisher_est_map, schedulers, scaler
 
     def learn(self, task_id: int, verbose=False):
         assert self.training_mode, "Agent not in training mode"
@@ -239,7 +243,13 @@ class HyperCEZAgent(Agent):
             reanalyze_state=self.make_ez_state(task_id, DecisionModel.REANALYZE)
         )
 
-        targets, theta_optimizers, emb_optimizers, reg_param_map, fisher_est_map, schedulers = self.cl_training_misc[task_id]
+        (targets,
+         theta_optimizers,
+         emb_optimizers,
+         reg_param_map,
+         fisher_est_map,
+         schedulers,
+         scaler) = self.cl_training_misc[task_id]
 
         # Adjust Learning Rates
         for component_name in self.hnet_component_names:
@@ -282,9 +292,92 @@ class HyperCEZAgent(Agent):
             self.__memory_manager.get_trained_steps(task_id),
         )
 
-        self.ez_agent.update_weights(weighted_loss, self.ez_agent.model, self.ez_agent.optimizer, self.ez_agent.scaler)
+        self.update_weights(weighted_loss, task_id)
         self.__memory_manager.increment_trained_steps(task_id)
 
+    def update_weights(self, weighted_loss, task_id: int):
+        (targets,
+         theta_optimizers,
+         emb_optimizers,
+         reg_param_map,
+         fisher_est_map,
+         schedulers,
+         scaler) = self.cl_training_misc[task_id]
+
+        with torch.amp.autocast(self.device.type):
+            weighted_loss.register_hook(lambda grad: grad * (1. / self.hparams.train["unroll_steps"]))
+
+        # Zero gradients
+        self.ez_agent.optimizer.zero_grad()
+        for component_name in self.hnet_component_names:
+            theta_optimizers[component_name].zero_grad()
+            emb_optimizers[component_name].zero_grad()
+
+        # scaled backward
+        scaler.scale(weighted_loss).backward(
+            retain_graph=task_id > 0 and self.hparams.beta > 0,
+            create_graph=self.hparams.backprop_dt and task_id > 0 and self.hparams.beta > 0
+        )
+
+        # unscale gradients
+        scaler.unscale_(self.ez_agent.optimizer)
+        for component_name in self.hnet_component_names:
+            # scaler.unscale_(theta_optimizers[component_name])
+            scaler.unscale_(emb_optimizers[component_name])
+
+        # clip gradients on embedding params and ezv2 params
+        torch.nn.utils.clip_grad_norm_(self.ez_agent.model.parameters(), self.hparams.train["max_grad_norm"])
+        for component_name in self.hnet_component_names:
+            torch.nn.utils.clip_grad_norm_(
+                self.hnet_map[component_name].get_task_emb(task_id), self.hparams.grad_max_norm
+            )
+
+        # step embedding optimizer and ezv2 optimizer
+        scaler.step(self.ez_agent.optimizer)
+        for component_name in self.hnet_component_names:
+            scaler.step(emb_optimizers[component_name])
 
 
+        # Compute Regularization loss
+        if task_id > 0 and self.hparams.beta > 0:
+            for component_name in self.hnet_component_names:
+                if self.hparams.no_look_ahead:
+                    d_theta = None
+                else:
+                    d_theta = opstep.calc_delta_theta(
+                        theta_optimizers[component_name],
+                        self.hparams.use_sgd_change,
+                        lr=self.hparams.lr_hyper,
+                        detach_dt=not self.hparams.backprop_dt
+                    )
 
+                if self.hparams.plastic_prev_tembs:
+                    d_tembs = d_theta[-task_id:]
+                    d_theta = d_theta[:-task_id] if d_theta is not None else None
+                else:
+                    d_tembs = None
+
+                loss_reg = hnet_regularizer.calc_fix_target_reg(
+                    self.hnet_map[component_name],
+                    task_id,
+                    targets=targets[component_name],
+                    dTheta=d_theta,
+                    dTembs=d_tembs,
+                    mnet=self.ez_agent.model if component_name == "singular" else self.ez_agent.model.__getattr__(component_name),
+                    fisher_estimates=fisher_est_map[component_name],
+                )
+
+                scaler.scale(loss_reg).backward()
+
+        # unscale, clip, step on theta optimizers
+        for component_name in self.hnet_component_names:
+            scaler.unscale_(theta_optimizers[component_name])
+            torch.nn.utils.clip_grad_norm_(reg_param_map[component_name], self.hparams.grad_max_norm)
+            scaler.step(theta_optimizers[component_name])
+
+        scaler.update()
+
+        if self.hparams.model["noisy_net"]:
+            self.ez_agent.model.value_policy_model.reset_noise()
+
+        return [scaler]
